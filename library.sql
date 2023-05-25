df9840b4e7ea5d2e923a8dff8866ade03b018994
Muhammad Abdullah <75414797+WhoAbdullahSheikh@users.noreply.github.com>
2023-05-25 09:13:29 +0500

    Add files via upload

---
+++ library.sql
@@ -0,0 +1,80 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: May 25, 2023 at 06:12 AM
+-- Server version: 10.4.21-MariaDB
+-- PHP Version: 8.0.10
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `library`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `books`
+--
+
+CREATE TABLE `books` (
+  `BID` int(255) DEFAULT NULL,
+  `BName` varchar(255) DEFAULT NULL,
+  `Genre` varchar(255) DEFAULT NULL,
+  `Price` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `books`
+--
+
+INSERT INTO `books` (`BID`, `BName`, `Genre`, `Price`) VALUES
+(231312, 'Alice and the wonderland', 'Adventure', '5000');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `issuebooks`
+--
+
+CREATE TABLE `issuebooks` (
+  `BID` int(255) DEFAULT NULL,
+  `BName` varchar(255) DEFAULT NULL,
+  `Genre` varchar(255) DEFAULT NULL,
+  `Price` int(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `issuebooks`
+--
+
+INSERT INTO `issuebooks` (`BID`, `BName`, `Genre`, `Price`) VALUES
+(231312, 'Alice and the wonderland', 'Adventure', 5000);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `Username` varchar(255) DEFAULT NULL,
+  `Password` varchar(255) DEFAULT NULL,
+  `UserID` varchar(255) DEFAULT NULL,
+  `Admin` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
